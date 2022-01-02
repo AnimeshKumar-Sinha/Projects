1c249038de7f8baaebad44840e84e13bf741f5e3
Animesh Sinha <animeshsinha@Animeshs-MBP-2.hsd1.ca.comcast.net>
2022-01-01 21:52:24 -0800

    igen-project

---
+++ snowflack-Idgen-ms/src/main/java/com/tcs/kogr/idgen/snowflackIdgenms/SnowflackIdgenMsApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.kogr.idgen.snowflackIdgenms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowflackIdgenMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowflackIdgenMsApplication.class, args);
+	}
+
+}
---
+++ snowflack-Idgen-ms/src/test/java/com/tcs/kogr/idgen/snowflackIdgenms/SnowflackIdgenMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.kogr.idgen.snowflackIdgenms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowflackIdgenMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
